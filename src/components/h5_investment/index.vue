eb9d354c02ee54cdaefb3e325dc14b65d953a640
hezihua <2283462371@qq.com>
2024-11-25 23:52:08 +0800

    feat: investment start

--- src/components/h5_investment/index.vue
+++ src/components/h5_investment/index.vue
@@ -1,30 +1,36 @@
-<script setup>
-import { ref } from 'vue'
-  import Header from '../common/h5_header.vue'
-
-defineProps({
-  msg: String,
-})
-
-const count = ref(0)
-</script>
-
 <template>
-  <div class="section">
+  <div ref="container" class="container">
     <Header />
-    h5_investment</div>
+    <Section1 />
+    <Section2 />
+    <Section3 />
+    <Section4 />
+    <Section5 />
+  </div>
 </template>
 
-<style scoped>
-.section {
-    height: 100vh;
-    width: 100vw;
-    color: #fff;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-size: 30rem;
-    position: relative;
-    background: #234c68;
+<script setup>
+import { onMounted, ref } from 'vue';
+import Header from '../common/h5_header.vue';
+import Section1 from './section1.vue';
+import Section2 from './section2.vue';
+import Section3 from './section3.vue';
+import Section4 from './section4.vue';
+import Section5 from './section5.vue';
+onMounted(() => {
+  
+  
+});
+</script>
+
+<style scoped lang="scss">
+
+.container {
+  background: #eee;
+  width: 100vw;
+
+  height: 520vh;
+  margin: 0 auto;
+  overflow: visible;
 }
 </style>
---
+++ src/components/h5_investment/section4.vue
@@ -0,0 +1,47 @@
+<!-- Section1.vue -->
+<template>
+    <div class="section section4">
+      <div class="content">Section 4 Content</div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Section1',
+  };
+  </script>
+  
+  <style scoped>
+  .section {
+    height: 100vh;
+    position: sticky;
+    overflow: hidden;
+    border-radius: 20rem 20rem 0 0;
+    position: sticky;
+    top: 0;
+  }
+  
+  .content {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 20rem;
+    color: white;
+    background-color: #4a90e2;
+    z-index: 4;
+    /* 使用 transform 控制内容动画 */
+    transition: transform 0.5s ease;
+  }
+  
+  /* 模拟后一个 section 的内容逐步覆盖当前 section */
+  .section + .section .content {
+    position: sticky;
+    top: 0;
+    height: 100vh;
+    background-color: green;
+    z-index: 5;
+  }
+  </style>
+  
