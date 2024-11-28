46496fc001bdda1f32e4c861cb38083f8c4d2ed8
hezihua <2283462371@qq.com>
2024-11-28 23:44:29 +0800

    feat: lenis 引用

--- src/components/pc_index/evolution.vue
+++ src/components/pc_index/evolution.vue
@@ -3,6 +3,7 @@ import { ref, onMounted, watch, onUnmounted } from 'vue';
 import Story from './story.vue'
 import gsap from "gsap";
   import ScrollTrigger from "gsap/ScrollTrigger";
+  import Lenis from 'lenis'
   
   // 注册 ScrollTrigger 插件
   gsap.registerPlugin(ScrollTrigger);
@@ -107,6 +108,26 @@ onMounted(() => {
   const timelinecontentModule = document.querySelector('.timelinecontent');
   timelineobserver.observe(timelinecontentModule);
 
+
+  // Initialize a new Lenis instance for smooth scrolling
+  const lenis = new Lenis();
+
+  // Listen for the 'scroll' event and log the event data to the console
+  lenis.on('scroll', (e) => {
+    console.log(e);
+  });
+
+  // Synchronize Lenis scrolling with GSAP's ScrollTrigger plugin
+  lenis.on('scroll', ScrollTrigger.update);
+
+  // Add Lenis's requestAnimationFrame (raf) method to GSAP's ticker
+  // This ensures Lenis's smooth scroll animation updates on each GSAP tick
+  gsap.ticker.add((time) => {
+    lenis.raf(time * 1000); // Convert time from seconds to milliseconds
+  });
+
+  // Disable lag smoothing in GSAP to prevent any delay in scroll animations
+  gsap.ticker.lagSmoothing(0);
    // 创建动画时间轴
    const timeline = gsap.timeline({
       scrollTrigger: {
@@ -121,7 +142,6 @@ onMounted(() => {
     // 添加动画效果
   timeline
     .to("#timeline-container", { translateX: '-100%' })         // 放大
-    // .to(".timeline-section", { opacity: 1, duration: 1000 })   
 });
 
 
