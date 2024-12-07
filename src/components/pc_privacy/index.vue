d40d514be64ff57606aba1017834d66f2be14ac0
hezihua <2283462371@qq.com>
2024-12-07 21:35:55 +0800

    feat: 协议优化

--- src/components/pc_privacy/index.vue
+++ src/components/pc_privacy/index.vue
@@ -5,148 +5,141 @@
             Privacy Policy
         </div>
         <div class="content">
-            Personal Information Collection Statement
-            <p>
+            <div class="sub">Personal Information Collection Statement</div>
+            <div class="item">
 
                 This Personal Information Collection Statement (the “Statement”) is made by Infini Capital Management Limited (the “Company”, “we”, “us”) pursuant to the Personal Data (Privacy) Ordinance of (Cap. 486) of the Laws of Hong Kong Special Administrative Region (“PDPO”). This Statement intends to notify you why your personal data is collected, how it will be used, to whom the personal data will be transferred and to whom data access requests are to be addressed. 
-            </p>
-            <div>
+            </div>
+            <div class="sub">
                 Collection
             </div>
-            <p>
-                <div>
+
+                <div class="item">
 
                     1.From time to time, we may collect your personal data in connection with various matters such as to facilitate the onboarding process with the fund manager or provide any asset management related services, or in compliance with any laws or guidelines issued by regulatory or other authorities. It is not mandatory for you to provide your personal data. In the event that you do not provide such personal data, the Company may not be able to provide some of the information, services and/or products of the Company.
                 </div>
-                <div>
+                <div class="item">
 
                     2.The kind of personal data that may be collected includes, but is not limited to, name and address, occupation, contact details, date of birth, marital status, nationality, identity card or passport numbers, details of financial and investment status.
                 </div>
-                <div>
+                <div class="item">
 
                     3.Provision of your personal data is voluntary. However, failure to supply the requested personal data may result in the Company being unable to provide you with any information, services and/or products of the Company or unable to comply with any laws or regulations or guidelines and codes issued by regulatory or other authorities.
                 </div>
-                <div>
+                <div class="item">
 
                     4.It is also the case that personal data is collected from you during any communication with the Company, for example, when you direct the Company to liaise with external parties (e.g. fund administrator) in relation to any general enquiries or generally communicate verbally or in writing with the Company.
                 </div>
-            </p>
-            <div>
+                <div class="sub">
                 Purpose and use
             </div>
-            <p>
-                <div>
+
+                <div class="item">
                     5.The purposes for which personal data may be used will vary depending on the nature of your relationship with the Company. The Company intends to use your personal data for any or all of the following purposes:
                 </div>
-                <div>
+                <div class="item">
                 a)Facilitating the processing of an application for an account including confirming and verifying your identity and your status as a “professional investor” (as defined under the Securities and Futures Ordinance of Hong Kong (Cap. 571) (“SFO”), and assisting with any relevant onboarding measures and AML/KYC processes as requested by the investment manager of the fund which you are subscribing for units or interests in;
                 </div>
-                <div>
+                <div class="item">
                 b)Any purpose related to the administration of products and services offered by the Company, and the acting as the local contact for the fund which you are subscribing for units or interests in;
                 </div>
-                <div>
+                <div class="item">
                 c)Researching, designing and launching financial, investment services or related products for clients;
                 </div>
-                <div>
+                <div class="item">
                 d)Promoting and the direct marketing of one or more of the following products or services: (i) collective investment schemes managed, advised, distributed or promoted by the Company and the investment manager of the fund which you are subscribing for units or interests in; (ii) asset management, investment advisory, wealth management or any other investment related services by the Company and the investment manager of the fund which you are subscribing for units or interests in; and (iii) any other financial or investment products or services offered by the investment manager of the fund which you are subscribing for units or interests in, subject to your right to object to such use;
                 </div>
-                <div>
+                <div class="item">
                 e)Meeting the disclosure, reporting and compliance obligations, requirements or arrangements of the Company and/or the respective fund manager and affiliates of the respective fund manager to comply with, or in connection with any laws, regulations or rules binding on the Company and/or the respective fund manager or any rules, codes, guidelines or directions issued by such legal, governmental, regulatory, tax, law enforcement or other authorities with which the Company and/or the respective fund manager (as applicable) is expected to comply, including, where applicable, for the purpose of assisting the respective fund manager that the Company provides services to in obtaining any exemption, reduction or refund of any withholding or other taxes imposed by any taxing authority or other government agency; and
                 </div>
-                <div>
+                <div class="item">
                 f)Any other purpose directly related to the above.
                 </div>
-                <div>
+                <div class="item">
                     6.You have the right to object to the use of your Data for direct marketing purposes as set out in paragraph 5(d) above. The Company will cease to use your Data for direct marketing purposes without charge if you so request.
                 </div>
-            </p>
-            <div>Retention, security, transfer and access</div>
+                <div class="sub">Retention, security, transfer and access</div>
 
-            <p>
-                <div>
+                <div class="item">
                     7.The personal data collected may be maintained for a reasonable time to fulfill the purpose for which the data was collected or comply with applicable rules, regulations and laws of Hong Kong Personal data which is no longer required will be destroyed in accordance with our internal retention period and will not be kept longer than necessary.
                 </div>
-                <div>
+                <div class="item">
                     8.Personal data held by us will be kept confidential and the Company will take all reasonable measures to ensure that personal data is kept secure and protected against unauthorized or accidental access, processing, loss, disclosure, destruction or other use.
                 </div>
-                <div>
+                <div class="item">
                     The Company may provide, or where necessary, facilitate the provision of such information to the following parties, whether inside or outside of Hong Kong, for the purposes set out in the “Purpose and Use” section above. Please note that such information may be transferred to a place outside Hong Kong, including but not limited to Japan and the U.S. You consent to us disclosing your personal data overseas to fund managers located outside of Hong Kong for this purpose.
                 </div>
-                <div>
+                <div class="item">
                     a)the ultimate holding company of the Company, its subsidiaries, representative offices, associated companies and/or affiliates as applicable;
                 </div>
-                <div>
+                <div class="item">
                     b)the service providers of the Company and the fund which you are subscribing to units or interests in, including (but not limited to) the trustee, investment manager, transfer agent, custodian, fund administrator and the auditor of each product offered by the Company; 
                 </div>
-                <div>
+                <div class="item">
                     c)any agent, contractor or third-party service provider who provides administrative, research, design, launch, digital or telecommunication, clearing and settlement or other services to the Company in connection with the operation of its business;
                 </div>
-                <div>
+                <div class="item">
                     d)the employees, officers, directors of the Company;
                 </div>
-                <div>
+                <div class="item">
                     e)any party (including but not limited to, any local or foreign governmental, tax or regulatory authority or court of competent jurisdiction) entitled thereto by law or regulation or in response to a regulatory request; and
                 </div>
-                <div>
+                <div class="item">
                     f)external service providers of the Company (including but not limited to digital or telecommunication companies, and information technology companies) that the Company engages for the purposes set out above.
                 </div>
-            </p>
 
-            <div>
+
+                <div class="sub">
                 Data Access and Correction
             </div>
-            <p>
-                <div>
+                <div class="item">
                     10.Under the PDPO, you have the right to:
                 </div>
-                <div>
+                <div class="item">
                     a)check whether the Company holds personal data about you, and access to personal data;
                 </div>
-                <div>
+                <div class="item">
                     b)require the Company to correct any personal data relating to you which is inaccurate;
                 </div>
-                <div>
+                <div class="item">
                     c)ascertain the Company’s policies and practices in relation to data and to be informed of the kinds of personal data held by the Company; and
                 </div>
-                <div>
+                <div class="item">
                     d)to the extent that the Company wishes to use your personal data for direct marketing purposes, object to the use of your personal data for such purposes or withdrawal of any consent and the Company shall not use your personal data in any way for direct marketing purposes after you communicate your objection or withdrawal to the Company, the first two above points being a “Data Access Request”.
                 </div>
-                <div>
+                <div class="item">
                     11.Upon receiving a Data Access Request, the Company will process it within a reasonable time. In accordance with the PDPO, the Company has the right to charge a reasonable fee for processing any Data Access Request and/or refuse a Data Access Request with reasons of refusal provided to you. You are entitled to object to a Data Access Request refusal from the Company.
                 </div>
-                <div>
-                    <div>
+                <div class="item">
 
-                        12.To submit a Data Access Request or for information regarding the Company’s policies and practices relating to personal data, please contact:
-                    </div>
-                    <div>
-
-                        Suites 1307-1308, 13/F
-                    </div>
-                    <div>
-
-                        Two Exchange Square
-                    </div>
-                    <div>
-
-                        8 Connaught Place, Central, Hong Kong 
-                    </div>
-                    <div>
-                        
-                        Attn: General Counsel
-                    </div>
-                    <div>
-
-                        Email: stephen.lukow@infinicapital.com
-                    </div>
+                    12.To submit a Data Access Request or for information regarding the Company’s policies and practices relating to personal data, please contact:
                 </div>
-                <div>
+                <div class="item">
+
+                    Suites 1307-1308, 13/F
+                </div>
+                <div class="item">
+
+                    Two Exchange Square
+                </div>
+                <div class="item">
+
+                    8 Connaught Place, Central, Hong Kong 
+                </div>
+                <div class="item">
+                    
+                    Attn: General Counsel
+                </div>
+                <div class="item">
+
+                    Email: stephen.lukow@infinicapital.com
+                </div>
+                <div class="item">
                     13.The Company reserves the right, at any time and without prior notice, to add to, change, update or modify this Statement.
                 </div>
-                <div>
+                <div class="item">
                     14.Nothing in this Statement shall limit your rights under the Ordinance.
                 </div>
-            </p>
         </div>
         <Footer />
     </div>
@@ -165,11 +158,19 @@ import Footer from '../common/footer.vue'
         font-size: 26rem;
         .title {
             font-size: 58rem;
-            margin-bottom: 20rem;
+            margin-bottom: 30rem;
             padding: 0 10vw 0;
         }
         .content {
             padding: 0 10vw 80rem;
+            .sub {
+                font-size: 38rem;
+                margin-bottom: 20rem;
+            }
+            .item {
+                margin: 0 0 10rem;
+                text-indent: 2em;
+            }
         }
     }
 </style>
