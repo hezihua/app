37981c82941be26150b2b4fdf9389824af1b2ebe
hezihua <2283462371@qq.com>
2024-12-08 22:14:07 +0800

    feat: pc 弹框开发

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,29 @@
 <script setup>
+import { onMounted, ref } from 'vue';
+import tips from './components/common/tips.vue'
+const showTips = ref(true)
+
+const handleAgree = ()=> {
+  showTips.value = false
+  var overlay = document.querySelector("#app");
+  overlay.style.overflow = "";
+  overlay.style.height = "";
+}
+
+const handleDisagree = ()=> {
+  window.location.href = 'about:blank';
+}
+onMounted(()=> {
+  var overlay = document.querySelector("#app");
+  overlay.style.overflow = "hidden";
+  overlay.style.height = "100vh";
+  // overlay.style.overflow = "hidden";
+})
 </script>
 
 <template>
-
   <router-view></router-view>
+  <tips :isVisible="showTips" @agree="handleAgree" @disagree="handleDisagree"/>
 </template>
 
 <style scoped>
