b2c800260f0e1fc555690eb9fedd03e8d7fb0f77
hezihua <2283462371@qq.com>
2024-12-09 21:48:08 +0800

    feat: 设置token 时效

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,29 @@
 import { onMounted, ref } from 'vue';
 import tips from './components/common/tips.vue'
 import h5_tips from './components/common/h5_tips.vue'
-const showTips = ref(true)
+const showTips = ref(false)
 
 const handleAgree = ()=> {
   showTips.value = false
-  // var overlay = document.querySelector("#app");
-  // overlay.style.overflow = "";
-  // overlay.style.height = "";
+  localStorage.setItem('token', new Date().getTime())
+  let overlay = document.querySelector("#app");
+  overlay.style.overflow = "";
+  overlay.style.height = "";
 }
 
 const handleDisagree = ()=> {
   window.location.href = 'about:blank';
 }
 onMounted(()=> {
-  // var overlay = document.querySelector("#app");
+  let overlay = document.querySelector("#app");
+  overlay.style.overflow = "hidden";
+  overlay.style.height = "100vh";
   // overlay.style.overflow = "hidden";
-  // overlay.style.height = "100vh";
-  // overlay.style.overflow = "hidden";
+  const token = localStorage.getItem('token')
+  console.log(token)
+  if(!token || new Date().getTime() > Number(token) + 3 * 24 * 60 * 60 * 1000) {
+    showTips.value = true
+  }
 })
 </script>
 
